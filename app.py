432fc9fe4a1771362610f6cd3665bcde09ecdf4d
Manoz Kshetri <152266540+DipsManoz@users.noreply.github.com>
2025-03-17 14:04:46 +0545

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import numpy as np
 import joblib 
 
 # Load YOLOv8 model (pre-trained on COCO dataset, which includes animals)
-model = YOLO('best.pt')
+model = YOLO('final_best.pt')
 
 # Function to detect animals in video
 def detect_animals(video_path):
